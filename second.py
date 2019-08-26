5916e0d42950795332512991cfa5c4abbe432770
de Souza <yara.de.souza@accenture.com>
2019-08-26 15:10:58 -0300

    routing

--- second.py
+++ second.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask import render_template
 
 app = Flask(__name__)
@@ -7,5 +7,14 @@ app = Flask(__name__)
 def ola():
 	# index.html follows jinja template
 	name = 'Mariana'
-	subjects = ["História", "Ingles", "Biologia"]
+	subjects = ["História", "Ingles", "Biologia", "Física"]
 	return  render_template("index.html", user_name = name, subjects = subjects)
+
+@app.route("/subject/<subject_name>")
+def subject(subject_name):
+	if (subject_name == "Biologia"):
+		return redirect("https://www.mcb.harvard.edu/")
+	elif (subject_name == "Ingles"):
+		return render_template(subject_name + ".html")
+	else:
+		return render_template( "defaultSubject.html", subject_name=subject_name)
